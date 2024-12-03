a51c16eb84ea78176f201f5128e62e18d6ed568f
elena-dogman <dogman.choli@gmail.com>
2024-12-03 14:30:01 +0300

    feat: initialize clerk

--- app/(auth)/(routes)/sign-in/page.tsx
+++
@@ -1,5 +0,0 @@
-const SignInPage = () => {
-  return <div>Unprotected</div>
-}
-
-export default SignInPage
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from '@clerk/nextjs'
+
 export default function Home() {
-  return <p className='text-3xl text-sky-700'>Protected</p>
+  return (
+    <div>
+      <UserButton />
+    </div>
+  )
 }
